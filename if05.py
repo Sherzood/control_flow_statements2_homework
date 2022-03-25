6a9b4bbd69ea04e7a70af514f8add58fc0a1735b
sherzod89 <sherzodms@inbox.ru>
2022-03-25 23:26:37 +0300

    01

--- if05.py
+++ if05.py
@@ -6,4 +6,22 @@ def main(n):
     Returns:
         int: return answer.
     """
-    return
+    n1=n%10
+    n//=10
+    n2=n%10
+    n//=10
+    n3=n%10
+    n//=10
+    n4=n%10
+    n5=n//10
+    mx=n1
+    if n2>mx:
+        mx=n2
+    if n3>mx:
+        mx=n3
+    if n4>mx:
+        mx=n4
+    if n5>mx:
+        mx=n5            
+    return mx
+print(main(12945))    
